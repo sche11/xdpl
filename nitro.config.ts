41136dedccbfa4851f68390c144abe267d999358
xixu-me <i@xi-xu.me>
2026-08-19 17:13:01 +0800

    feat(proxy): add Google Translate route

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,6 +2,9 @@
 export default defineNitroConfig({
   compatibilityDate: "2026-03-17",
   routeRules: {
+    "/google/**": {
+      proxy: "https://translate.googleapis.com/**",
+    },
     "/**": {
       proxy: "https://www2.deepl.com/**",
     },
